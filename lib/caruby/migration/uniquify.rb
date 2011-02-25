e80721a1a3869dedf446b8708efadeaf3ded3e52
Fred Loney <loneyf@ohsu.edu>
2011-02-25 15:43:24 -0800

    Augment Resource::Unique module directly

--- lib/caruby/migration/uniquify.rb
+++ lib/caruby/migration/uniquify.rb
@@ -2,14 +2,12 @@ require 'caruby/migration/migratable'
 require 'caruby/domain/uniquify'
 
 module CaRuby
-  module Migratable
-    # Unique makes a Migratable Resource domain object unique within the scope its class.
+  module Resource
     module Unique
-      include CaRuby::Resource::Unique
-      
-      # Augments the migration by making this Resource object unique in the scope of its class.
+      # Augments this {Unique} mix-in with a {Migratable#migrate} method which calls {Unique#uniquify}
+      # to make this Resource object unique in the scope of its class.
       #
-      # @param (see CaRuby::Migratable#migrate)
+      # @param (see Migratable#migrate)
       def migrate(row, migrated)
         super
         logger.debug { "Migrator making #{self} unique..." }
