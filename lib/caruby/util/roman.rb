20a8e9c447e03d570c5808e38bc9744cb21ca5c1
Fred Loney <loneyf@ohsu.edu>
2011-02-28 14:02:23 -0800

    Add roman-arabic converter for pathology scores.

---
+++ lib/caruby/util/roman.rb
@@ -0,0 +1,28 @@
+class String
+  # @return [Integer] the integer equivalent of this roman numeral
+  # @raise ArgumentError if this String is not a roman numeral in the range I-X
+  def to_arabic
+    case self
+      when /^(I{0,3})$/ then $1.size
+      when /^(I{0,3})(V|X)$/ then ROMAN_UNITS[$2] - $1.size
+      when /^(V)(I{0,3})$/ then ROMAN_UNITS[$1] + $2.size
+      else raise ArgumentError.new("#{self} is not a roman numeral in the range I-X")
+    end
+  end
+  
+  private
+  
+  ROMAN_UNITS = {'I' => 1, 'V' => 5, 'X' => 10}
+end
+
+class Integer
+  # @return [String] the roman numeral equivalent of this integer
+  def to_roman
+    if self < 1 or self > 10 then raise ArgumentError.new("#{self} cannot be converted to a roman numeral in the range I-X")
+    elsif self < 4 then 'I' * self
+    elsif self < 6 then ('I' * (5 - self)) + 'V'
+    elsif self < 9 then 'V' + ('I' * (self - 5))
+    else ('I' * (10 - self)) + 'X'
+    end
+  end
+end
---
+++ test/lib/caruby/util/roman_test.rb
@@ -0,0 +1,18 @@
+$:.unshift 'lib'
+
+require "test/unit"
+require 'caruby/util/roman'
+
+class RomanTest < Test::Unit::TestCase
+  def test_to_arabic
+    ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X' ].each_with_index do |s, n|
+      assert_equal(n + 1, s.to_arabic, "Conversion of #{s} incorrect")
+    end
+  end
+  
+  def test_to_roman
+    ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X' ].each_with_index do |s, n|
+      assert_equal(s, (n + 1).to_roman, "Conversion of #{n + 1} incorrect")
+    end
+  end
+end
