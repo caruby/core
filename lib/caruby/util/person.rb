833f610038a1d9120858fe419a243f945bf43063
Fred Loney <loneyf@ohsu.edu>
2011-04-25 15:00:02 -0700

    Add comments.

--- lib/caruby/util/person.rb
+++ lib/caruby/util/person.rb
@@ -4,12 +4,29 @@ require 'caruby/util/validation'
 module CaRuby
   module Person
     class Name
-      include Validation
-
-      attr_accessor :salutation, :qualifier, :credentials
-      attr_reader :first, :last, :middle
+      # @return [String] the salutation, e.g. 'Mr.'
+      attr_accessor :salutation
+      
+      # @return [String] the trailing name qualifier, e.g. 'III'
+      attr_accessor :qualifier
+      
+      # @return [String] the trailing name credentials, e.g. 'MD'
+      attr_accessor :credentials
+      
+      # @return [String] the first name
+      attr_reader :first
+      
+      # @return [String] the middle name
+      attr_reader :middle
+      
+      # @return [String] the ;ast name
+      attr_reader :last
 
       # Creates a new Name with the required last and optional first and middle components.
+      #
+      # @param [String] last the required last name
+      # @param [String, nil] first the optional first name
+      # @param [String, nil] middle the optional middle name
       def initialize(last, first=nil, middle=nil)
         # replace empty with nil
         @first = first unless first == ''
@@ -17,7 +34,8 @@ module CaRuby
         @middle = middle unless middle == ''
       end
 
-      # Returns this Name as an array consisting of the first, middle and last fields.
+      # @return [(String, String, String)] this Name as an array consisting of the first,
+      # middle and last fields
       #
       # @example
       #  Person.parse("Abe Lincoln").to_a #=> ["Abe", nil, "Lincoln"]
@@ -25,14 +43,14 @@ module CaRuby
         [@first, @middle, @last]
       end
 
-      # Returns this Name in the format [Salutation] First [Middle] Last[, Credentials].
+      # @return [String] this Name in the format [Salutation] First [Middle] Last[, Credentials]
       def to_s
         name_s = [salutation, first, middle, last, qualifier].reject { |part| part.nil? }.join(' ')
         name_s << ', ' << credentials if credentials
         name_s
       end
 
-      # Returns whether this Person's first, middle and last name components equal the other Person's.
+      # @return [Boolean] whether this Person's first, middle and last name components equal the other Person's
       def ==(other)
         self.class == other.class and first == other.first and middle == other.middle and last == other.last
       end
@@ -44,12 +62,15 @@ module CaRuby
       # * [salutation] [first [middle]] last [qualifier] [, credentials]
       # where _salutation_ ends in a period.
       #
-      # Examples:
+      # Example input:
       # * Longfellow, Henry Wadsworth
       # * Longfellow, Henry Gallifant Wadsworth
       # * Henry Longfellow
       # * Longfellow
       # * Mr. Henry Wadsworth Longfellow III, MD, Ph.D.
+      #
+      # @param [String] name_s the name to parse
+      # @return [Name] the parsed Name
       def self.parse(name_s)
         return if name_s.blank?
         # the name component variables
@@ -94,8 +115,8 @@ module CaRuby
         name
       end
 
-      # Raises ValidationError if there is neither a first nor a last name
-      # or if there is a middle name but no first name.
+      # @raise [ValidationError] if there is neither a first nor a last name
+      # or if there is a middle name but no first name
       def validate
         if last.nil? and first.nil? then
           raise ValidationError.new("Name is missing both the first and last fields")
@@ -105,12 +126,20 @@ module CaRuby
         end
       end
 
-      # Returns whether s ends in a period.
+      # @param [String] s a name fragment
+      # @return [Boolean] whether the fragment ends in a period
       def self.salutation?(s)
         s =~ /\.$/
       end
 
-      # Returns whether s is Jr., Sr., II, III, etc.
+      # Returns whether the given name fragment is a recognized qualifier. The following
+      # qualifiers are recognized:
+      # * +Jr+ with optional period
+      # * +Sr+ with optional period
+      # * +I+, +II+ or +III+
+      #
+      # @param [String] s a name fragment
+      # @return [Boolean] whether s is a recognized qualifier
       def self.qualifier?(s)
         s and (s =~ /[J|S]r[.]?/ or s =~ /\AI+\Z/)
       end
