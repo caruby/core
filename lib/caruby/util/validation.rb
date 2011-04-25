8c53b417d3b7d23f7af642ac29749367d14c35b6
Fred Loney <loneyf@ohsu.edu>
2011-04-25 15:00:55 -0700

    Remove extraneous Validation mix-in.

--- lib/caruby/util/validation.rb
+++ lib/caruby/util/validation.rb
@@ -18,31 +18,3 @@ class Object
     nil? or (respond_to?(:empty?) and empty?)
   end
 end
-
-module Validation
-  # A Validator is a procedure which responds to the +validate(value)+ method.
-  class Validator < Proc
-    alias :validate :call
-  end
-
-  # Validates that each key value in the value_type_assns value => type hash is an instance of the associated class.
-  #
-  # Raises ValidationError if the value is missing.
-  # Raises TypeError if the value is not the specified type.
-  def validate_type(value_type_assns)
-    TYPE_VALIDATOR.validate(value_type_assns)
-  end
-
-  private
-
-  def self.create_type_validator
-    Validator.new do |value_type_assns|
-      value_type_assns.each do |value, type|
-        raise ArgumentError.new("Missing #{type.name} argument") if value.nil?
-        raise TypeError.new("Unsupported argument type; expected: #{type.name} found: #{value.class.name}") unless type === value
-      end
-    end
-  end
-
-  TYPE_VALIDATOR = create_type_validator
-end
