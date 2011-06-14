21e55d4e0980993fec4f1aee6cae7eef4c8f8792
Fred Loney <loneyf@ohsu.edu>
2011-06-14 16:52:17 -0700

    Add parent_module test cases.

--- test/lib/caruby/util/module_test.rb
+++ test/lib/caruby/util/module_test.rb
@@ -5,25 +5,21 @@ require 'caruby/util/module'
 
 module Outer
   module Middle
-    module InnerModule; end
-    class InnerClass; end
+    class C; end
   end
 end
 
 class ModuleTest < Test::Unit::TestCase
-  def test_top_level_module_with_name
-    assert_equal(Array, Module.module_with_name(nil, 'Array'), "Top level module incorrect")
-  end
-
-  def test_module_with_unqualified_name
+  def test_module_with_name
     assert_equal(Outer::Middle, Outer.module_with_name('Middle'), "Unqualified module incorrect")
+    assert_nil(Outer.module_with_name('Zed'), "Missing module incorrectly resolves to non-nil value")
+    assert_equal(Outer::Middle::C, Outer.module_with_name('Middle::C'), "Qualified module incorrect")
+    assert_equal(Outer, Kernel.module_with_name('Outer'), "Top-level module incorrect")
   end
-
-  def test_module_with_qualified_name
-    assert_equal(Outer::Middle::InnerModule, Outer.module_with_name('Middle::InnerModule'), "Qualified module incorrect")
-  end
-
-  def test_class_with_name
-    assert_equal(Outer::Middle::InnerClass, Outer.module_with_name('Middle::InnerClass'), "Inner class incorrect")
+  
+  def test_parent_module
+    assert_equal(Outer, Outer::Middle.parent_module, "Middle parent module incorrect")
+    assert_equal(Outer::Middle, Outer::Middle::C.parent_module, "Inner parent module incorrect")
+    assert_equal(Kernel, Outer.parent_module, "Outer parent module incorrect")
   end
 end
