5208c0192d16b80987f2f46d51716e66b5579f3b
Fred Loney <loneyf@ohsu.edu>
2011-06-14 17:08:16 -0700

    Add Galena filter shims.

---
+++ examples/galena/lib/galena/clinical_trials/migration/filter_shims.rb
@@ -0,0 +1,12 @@
+module ClinicalTrials
+
+  # Declares the classes modified for migration.
+  shims Participant
+
+  class Participant
+    # Transforms the Participant +SSN+ input field to an integer.
+    def migrate_ssn(value, row)
+      String === value ? value.split('-').join.to_i : value
+    end
+  end
+end
