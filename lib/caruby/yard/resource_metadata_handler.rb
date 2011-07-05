821d631c8b3f29ce0ac00027bc0ab8f7af166b61
Fred Loney <loneyf@ohsu.edu>
2011-07-05 16:25:18 -0700

    Remove unused class.

--- lib/caruby/yard/resource_metadata_handler.rb
+++
@@ -1,8 +0,0 @@
-class MetadataHandler < YARD::Handlers::Ruby::Legacy::AttributeHandler
-  handles method_call(/\Aqualify_attribute\b/)
-  namespace_only
-
-  def process
-    push_state(:scope => :class) { super }
-  end
-end
