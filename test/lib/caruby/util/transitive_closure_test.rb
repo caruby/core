865f1516d3fe5f34bf16149b201ab7aadf00cc24
Fred Loney <loneyf@ohsu.edu>
2011-08-03 13:28:57 -0700

    Refactor verify_closure.

--- test/lib/caruby/util/transitive_closure_test.rb
+++ test/lib/caruby/util/transitive_closure_test.rb
@@ -23,37 +23,45 @@ class TransitiveClosureTest < Test::Unit::TestCase
     alias :inspect :to_s
   end
 
+  # Verifies closure iteration for the following hierarchy:
+  #  root -> a, e
+  #  a -> b, c
+  #  c -> d
+  # The expected iteration is +root+ preceding +a+ and +e+, +a+ preceding +b+ and +c+, +c+ preceding +d+.
   def test_hierarchy
     root= Node.new('root'); a = Node.new('a', root); b = Node.new('b', a); c = Node.new('c', a); d = Node.new('d', c); e = Node.new('e', root)
-    expected = [root, a, b, c, d, e].to_set
-    closure = root.transitive_closure(:children)
-    assert_equal(expected, closure.to_set, "Hierarchy closure incorrect")
-    closure.each_with_index { |node, index| closure[index..-1].each { |other| assert(!other.children.include?(node), "Order incorrect") } }
-    closure.reject { |node| node.parent.nil? }.each_with_index { |node, index| assert(closure[0..index].detect { |other| other.children.include?(node) }, "Order incorrect") }
+    verify_closure([root, a, b, c, d, e], root.transitive_closure(:children))
   end
 
   def test_internal
     root= Node.new('root'); a = Node.new('a', root); b = Node.new('b', a); c = Node.new('c', a); d = Node.new('d', c); e = Node.new('e', root)
-    expected = [a, b, c, d].to_set
-    closure = a.transitive_closure(:children)
-    assert_equal(expected, closure.to_set, "Hierarchy closure incorrect")
+    verify_closure([a, b, c, d], a.transitive_closure(:children))
   end
 
   def test_leaf
     leaf = Node.new(1)
-    assert_equal([leaf], leaf.transitive_closure(:children), "Leaf closure incorrect")
+    verify_closure([leaf], leaf.transitive_closure(:children))
   end
 
   def test_collection
     a = Node.new('a'); b = Node.new('b'); c = Node.new('c', a); d = Node.new('d', b); e = Node.new('e', c)
-    expected = [a, b, c, d, e].to_set
-    closure = [a, b].transitive_closure(:children)
-    assert_equal(expected, closure.to_set, "Hierarchy closure incorrect")
+    verify_closure([a, b, c, d, e], [a, b].transitive_closure(:children))
   end
 
   def test_cycle
     root= Node.new('root'); a = Node.new('a', root); b = Node.new('b', a); c = Node.new('c', a); c.children << root
     expected = [root, a, b, c].to_set
-    assert_equal(expected, a.transitive_closure(:children).to_set, "Cycle closure incorrect")
+    verify_closure([root, a, b, c], root.transitive_closure(:children))
+  end
+  
+  def verify_closure(content, closure)
+    assert_equal(content.to_set, closure.to_set, "Hierarchy closure incorrect")
+    # Verify that no child succeeds the parent.
+    closure.each_with_index do |node, index|
+      par = node.parent
+      if content.include?(par) then
+        assert(closure.index(par) < index, "Child #{node} precedes parent #{par}")
+      end
+    end
   end
 end
