8240942205e7c2ca34953ae7097c637b016a1453
Fred Loney <loneyf@ohsu.edu>
2011-08-03 13:28:08 -0700

    Add rubydoc.

--- lib/caruby/util/transitive_closure.rb
+++ lib/caruby/util/transitive_closure.rb
@@ -1,7 +1,18 @@
 require 'caruby/util/visitor'
 
 class Object
-  # Returns the transitive closure over a method or block, e.g.:
+  # Returns the transitive closure over a method or block. This method returns an array partially ordered
+  # by the children method or block, i.e. each node occurs before all other nodes referenced directly or
+  # indirectly by the children.
+  #
+  # If a method symbol or name is provided, then that method is called. Otherwise, the block is called.
+  # In either case, the call is expected to return an object or Enumerable of objects which also respond
+  # to the method or block.
+  #
+  # @param [Symbol, nil] method the child reference, or nil if a block is given
+  # @yield [node] the parent node's children
+  # @yieldparam node the parent node
+  # @example
   #   class Node
   #     attr_reader :parent, :children
   #     def initialize(name, parent=nil)
@@ -17,16 +28,15 @@ class Object
   #   a = Node.new('a'); b = Node.new('b', a), c = Node.new('c', a); d = Node.new('d', c)
   #   a.transitive_closure { |node| node.children }.to_a.join(", ") #=> a, b, c, d
   #   a.transitive_closure(:children).to_a.join(", ") #=> a, b, c, d
-  # This method returns an array partially ordered by the closure method, i.e.
-  # each node occurs before all other nodes referenced directly or indirectly by the closure method.
-  #
-  # If a method symbol or name is provided, then that method is called. Otherwise, the block is called.
-  # In either case, the call is expected to return an object or Enumerable of objects which also respond
-  # to the method or block.
   def transitive_closure(method=nil)
     raise ArgumentError.new("Missing both a method argument and a block") if method.nil? and not block_given?
-    return transitive_closure() { |node| node.send(method) } if method
-    CaRuby::Visitor.new(:depth_first) { |node| yield node }.to_enum(self).to_a.reverse
+    # If there is a method argument, then the transitive closure is based on that method.
+    # Otherwise, visit the closure in reverse depth-first order.
+    if method then
+      transitive_closure() { |node| node.send(method) }
+    else
+      CaRuby::Visitor.new(:depth_first) { |node| yield node }.to_enum(self).to_a.reverse
+    end
   end
 end
 
