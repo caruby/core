2f58dd428b57fe323699d2c0d61d5bd3ca473478
Fred Loney <loneyf@ohsu.edu>
2011-09-22 17:13:27 -0700

    Remove unused code.

--- lib/caruby/domain.rb
+++ lib/caruby/domain.rb
@@ -97,92 +97,9 @@ module CaRuby
       # Loading the access properties on demand sets the classpath.
       access_properties
     end
-    
-#    # @param [Class, String] class_or_name the class to import into this module
-#    # @return [Class] the imported {Resource} class
-#    def resource_import(class_or_name)
-#      java_import(class_or_name)
-#      klass = Class.to_ruby(class_or_name)
-#      add_class(klass)
-#      klass
-#    end
-#
-#    # @param [String] the class name to check
-#    # @eturn [Class, nil] the domain class for the class name, or nil if none in this module
-#    def domain_type_with_name(name)
-#      pkg, base = Java.split_class_name(name)
-#      return unless pkg.nil? or pkg == @java_package
-#      begin
-#        type = const_get(base)
-#      rescue JavaImportError => e
-#        # no such domain type; not an error.
-#        # other exceptions indicate that there was a domain type but could not be loaded; these exceptions propagate up the call stack
-#        logger.debug { "#{$!}" }
-#        return
-#      end
-#      type if type < Resource
-#    end
 
     private
-    
-#    # Adds the given class to this domain module. The class is extended with {Metadata} methods.
-#    #
-#    # @param [Class] the {Resource} class to add
-#    def add_class(klass)
-#      # This domain module's known Resource classes.
-#      @rsc_classes ||= Set.new
-#      # Bail if already added.
-#      return if @rsc_classes.include?(klass)
-#
-#      logger.debug { "Adding #{klass.java_class.name} to #{qp}..." }
-#      # Add metadata to the class. This is done before adding the superclass and
-#      # referenced metadata, since they in turn might reference the current class
-#      # metadata.
-#      Metadata.extend_class(klass, self)
-#      # Add the class to the known Resource class set.
-#      @rsc_classes << klass
-#      
-#      # Make the superclass a Resource, if necessary.
-#      sc = klass.superclass
-#      unless @rsc_classes.include?(sc) then
-#        # the domain module includes the superclass on demand
-#        pkg, sym = Java.split_class_name(sc)
-#        if pkg == @java_package then
-#          # Load the superclass on demand; don't need to make this class a Resource,
-#          # but do need to import the class.
-#          const_get(sym)
-#        else
-#          # Superclass is not a member of the domain package; make this class a Resource.
-#          mod = @mixin
-#          klass.class_eval { include mod }
-#        end
-#      end
-#
-#      # Add referenced domain classes as necessary.
-#      klass.each_attribute_metadata do |attr_md|
-#        ref = attr_md.type
-#        next if @rsc_classes.include?(ref)
-#        pkg, sym = Java.split_class_name(ref)
-#        # This domain module adds a referenced Domain class on demand.
-#        if pkg == @java_package then
-#          puts "rm ac1 #{klass} #{attr_md} -> #{ref}..."
-#          logger.debug { "Loading #{klass.qp} #{attr_md} reference #{ref.qp}" }
-#          const_get(sym)
-#        end
-#      end
-#      
-#      # Invoke the callback.
-#      class_added(klass)
-#      logger.debug { "#{klass.java_class.name} added to #{qp}." }
-#      # print the class structure to the log
-#      logger.info(klass.pp_s)
-#    end
-#    
-#    # Callback invoked after the given domain class is added to this domain module.
-#    #
-#    # @param [Class] klass the class that was added
-#    def class_added(klass); end
-   
+       
     # Loads the application start-up properties in the given file path.
     #
     # @return (see #access_properties)
@@ -248,70 +165,6 @@ module CaRuby
         value
       end
     end
-#    
-#    # Imports the domain Java class with specified class name_or_sym.
-#    # This method enables the domain class extensions for storing and retrieving domain objects.
-#    # The class is added to this module.
-#    #
-#    # The optional block overrides the native Java property access wrappers.
-#    # For example:
-#    #   ClinicalTrials.java_import Java::edu.wustl.catissuecore.domain.Study do
-#    #     def study_code=(value)
-#    #       value = value.to_s if Integer === value
-#    #       setStudyCode(value)
-#    #     end
-#    #   end
-#    # imports the ClinicalTrials Study class as ClinicalTrials::Study and overrides the
-#    # +study_code+ setter method.
-#    #
-#    # Convenience aliases are added to the imported class, e.g. attribute +studyParticipantCollection+
-#    # is aliased as +study_participants+. Specifically, each attribute reader and writer is aliased with
-#    # the lower-case, underscore equivalent and a name ending in 'Collection' is changed to plural.
-#    # Pluralization is smart, e.g. +studyCollection+ is aliased to +studies+ rather than +studys+.
-#    #
-#    # The optional aliases argument consists of additional alias => standard attribute associations.
-#    # The optional owner_attr argument is a non-Java annotation owner attribute.
-#    #
-#    # @param [Symbol] symbol the class symbol
-#    # @param [String, nil] pkg the Java class package name, or nil for the default module package
-#    # @return [Class] the imported domain class
-#    def import_domain_class(symbol, pkg=nil)
-#      # import the Java class
-#      pkg ||= @java_package
-#      name = [pkg, symbol.to_s].join('.')
-#      logger.debug { "Detecting whether #{symbol} is a #{pkg} Java class..." }
-#      # Push each imported class onto a stack. When all referenced classes are imported,
-#      # each class on the stack is post-initialized and the class structure is printed to
-#      # the log.
-#      @import_stack ||= []
-#      @import_stack.push(symbol)
-#      begin
-#        resource_import(name)
-#      ensure
-#        @import_stack.pop
-#      end
-#      
-#      # the imported Java class is registered as a constant in this module
-#      klass = const_get(symbol)
-#      add_class(klass)
-#      
-#      # if we are back to top of the stack, then print the imported Resources
-#      if symbol == @import_stack.first then
-#        # a referenced class could be imported on demand in the course of printing a referencing class;
-#        # the referenced class is then pushed onto the stack. thus, the stack can grow during the
-#        # course of printing, but each imported class is consumed and printed in the end.
-#        until @import_stack.empty? do
-#          # the class constant
-#          sym = @import_stack.pop
-#          # the imported class
-#          kls = const_get(sym)
-#          # print the class structure to the log
-#          logger.info(kls.pp_s)
-#        end
-#      end
-#      
-#      klass
-#    end
 
     # The property/value matcher, e.g.:
     #   host: jacardi
