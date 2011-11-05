f28f2f240d2d65197ccb55f17b93991ec613000b
Fred Loney <loneyf@ohsu.edu>
2011-11-04 20:01:28 -0700

    Add mock fetch_association.

--- examples/clinical_trials/lib/clinical_trials/database.rb
+++ examples/clinical_trials/lib/clinical_trials/database.rb
@@ -7,5 +7,13 @@ module ClinicalTrials
   # The example mock database.
   class Database < CaRuby::Database
     include Singleton, CaRuby::Database::Persistifier
+
+    # @return a new instance of the target attribute class
+    # @see {CaRuby::Reader#fetch_association}
+    def fetch_association(obj, attribute)
+      attr_md = obj.class.attribute_metadata(attribute)
+      ref = attr_md.type.new
+      attr_md.collection? ? [ref] : ref
+    end
   end
 end
