de54728d01625ecd12ca466d34814e5e125bea16
Fred Loney <loneyf@ohsu.edu>
2011-11-07 18:06:32 -0800

    Refactor out class path modifier.

--- lib/caruby/import/java.rb
+++ lib/caruby/import/java.rb
@@ -8,62 +8,17 @@ require 'ftools'
 require 'date'
 
 require 'caruby/util/class'
-require 'caruby/util/log'
+require 'caruby/util/class_path_modifier'
 require 'caruby/util/inflector'
 require 'caruby/util/collection'
 
 module Java
-  private
+  @cp_mod = CaRuby::ClassPathModifier.new
   
-  # The Windows semi-colon path separator.
-  WINDOWS_PATH_SEP = ';'
-  
-  # The Unix colon path separator.
-  UNIX_PATH_SEP = ':'
-  
-  public
-  
-  # Adds the directories in the given path and all Java jar files contained in the directories
-  # to the execution classpath.
-  #
-  # @param [String] path the colon or semi-colon separated directories
+  # @see ClassPathModifier#add_path
   def self.add_path(path)
-    # the path separator
-    sep = path[WINDOWS_PATH_SEP] ? WINDOWS_PATH_SEP : UNIX_PATH_SEP
-    # the path directories
-    dirs = path.split(sep).map { |dir| File.expand_path(dir) }
-    # Add all jars found anywhere within the directories to the the classpath.
-    add_jars(*dirs)
-    # Add the directories to the the classpath.
-    dirs.each { |dir| add_to_classpath(dir) }
+    @cp_mod.add_path(path)
   end  
-  
-  # Adds the jars in the directories to the execution class path.
-  #
-  # @param [<String>] directories the directories containing jars to add
-  def self.add_jars(*directories)
-    directories.each do |dir|
-      Dir[File.join(dir , "**", "*.jar")].each { |jar| add_to_classpath(jar) }
-    end
-  end
-  
-  # Adds the given jar file or directory to the classpath.
-  #
-  # @param [String] file the jar file or directory to add
-  def self.add_to_classpath(file)
-    unless File.exist?(file) then
-      logger.warn("File to place on Java classpath does not exist: #{file}")
-      return
-    end
-    if file =~ /.jar$/ then
-      # require is preferred to classpath append for a jar file
-      require file
-    else
-      # A directory must end in a slash since JRuby uses an URLClassLoader.
-      if File.directory?(file) and not file =~ /\/$/ then file = file + '/' end
-      $CLASSPATH << file
-    end
-  end
 
   module JavaUtil
     # Aliases Java Collection methods with the standard Ruby Set counterpart, e.g. +delete+ for +remove+.
@@ -249,7 +204,7 @@ module Java
   end
   
   private
-  
+
   NAME_SPLITTER_REGEX = /^([\w.]+)\.(\w+)$/
 end
 
---
+++ lib/caruby/util/class_path_modifier.rb
@@ -0,0 +1,54 @@
+module CaRuby
+  # Helper class which adds files to the Java class path.
+  class ClassPathModifier
+    # Adds the directories in the given path and all Java jar files contained in the directories
+    # to the execution classpath.
+    #
+    # @param [String] path the colon or semi-colon separated directories
+    def add_path(path)
+      # the path separator
+      sep = path[WINDOWS_PATH_SEP] ? WINDOWS_PATH_SEP : UNIX_PATH_SEP
+      # the path directories
+      dirs = path.split(sep).map { |dir| File.expand_path(dir) }
+      # Add all jars found anywhere within the directories to the classpath.
+      add_jars(*dirs)
+      # Add the directories to the the classpath.
+      dirs.each { |dir| add_to_classpath(dir) }
+    end  
+
+    private
+  
+    # The Windows semi-colon path separator.
+    WINDOWS_PATH_SEP = ';'
+  
+    # The Unix colon path separator.
+    UNIX_PATH_SEP = ':'
+    
+    # Adds the jars in the directories to the execution class path.
+    #
+    # @param [<String>] directories the directories containing jars to add
+    def add_jars(*directories)
+      directories.each do |dir|
+        Dir[File.join(dir , "**", "*.jar")].each { |jar| add_to_classpath(jar) }
+      end
+    end
+
+    # Adds the given jar file or directory to the classpath.
+    #
+    # @param [String] file the jar file or directory to add
+    def add_to_classpath(file)
+      unless File.exist?(file) then
+        logger.warn("File to place on Java classpath does not exist: #{file}")
+        return
+      end
+      if file =~ /.jar$/ then
+        # require is preferred to classpath append for a jar file
+        require file
+      else
+        # A directory must end in a slash since JRuby uses an URLClassLoader.
+        if File.directory?(file) and not file =~ /\/$/ then file = file + '/' end
+        $CLASSPATH << file
+      end
+    end
+  end
+end  
