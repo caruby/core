344abbee260175755d89d5cf2ca0f0ce3b04a9a2
Fred Loney <loneyf@ohsu.edu>
2011-11-18 18:14:55 -0800

    Clarify nil_or_empty?.

--- lib/caruby/helpers/validation.rb
+++ lib/caruby/helpers/validation.rb
@@ -3,18 +3,29 @@ class ValidationError < RuntimeError; end
 
 class Object
   # Returns whether this object is nil, false, empty, or a whitespace string.
-  # For example, "", "   ", +nil+, [], and {} are blank.
-  #
   # This method is borrowed from Rails ActiveSupport.
+  #
+  # @example
+  #   ''.blank? => true
+  #   nil.blank? => true
+  #   false.blank? => true
+  #   [].blank? => true
+  #   [[]].blank? => false
+  # @return [Boolean] whether this object is nil, false, empty, or a whitespace string
+  # @see {#nil_or_empty?}
   def blank?
     respond_to?(:empty?) ? empty? : !self
   end
 
   # Returns whether this object is nil, empty, or a whitespace string.
-  # For example, "", "   ", +nil+, [], and {} return +true+.
+  # This method differs from {#blank?} in that +false+ is an allowed value.
   #
-  # This method differs from blank? in that +false+ is an allowed value.
+  # @example
+  #   ''.nil_or_empty? => true
+  #   nil.nil_or_empty? => true
+  #   false.nil_or_empty? => false
+  # @return [Boolean] whether this object is nil, empty, or a whitespace string
   def nil_or_empty?
-    nil? or (respond_to?(:empty?) and empty?)
+    blank? and self != false
   end
 end
