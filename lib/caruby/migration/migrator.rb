43995f7b726dd1be12280ec0506ab3fcb130b931
Fred Loney <loneyf@ohsu.edu>
2012-05-02 17:16:59 -0700

    Don't update an existing non-target migrated object.

--- lib/caruby/migration/migrator.rb
+++ lib/caruby/migration/migrator.rb
@@ -5,29 +5,45 @@ module CaRuby
   class Migrator < Jinx::Migrator
     # Creates a new Migrator with the given options.
     #
-    # This migrator must include sufficient information to build a well-formed migration target object.
-    # For example, if the target object is a new SpecimenCollectionGroup, then the migration must also be
-    # able to build that SCG's CollectionProtocolRegistration. The CPR in turn must either exist in the
-    # database or the migration must build a Participant and a CollectionProtocol.
+    # The migration configuration must provide sufficient information to build a well-formed migration
+    # target object. For example, if the target object is a new +CaTissue::SpecimenCollectionGroup+,
+    # then the migrator must build that SCG's required +CollectionProtocolRegistration+. The CPR in
+    # turn must either exist in the database or the migrator must build the required CPR
+    # +participant+ and +collection_protocol+.
     # 
     # @option (see Jinx::Migrator#initialize)
     # @option opts [Database] :database the target application database
+    # @see #migrate_to_database
     def initialize(opts={})
       super
       @database = opts[:database]
     end
     
     # Imports this migrator's file into the database with the given connect options.
-    # This method creates or updates the domain objects mapped from the import source.
-    # If a block is given to this method, then the block is called on each stored
-    # migration target object.
+    # This method creates or updates the domain objects mapped from the migration source.
+    # If a block is given to this method, then the block is called on each migrated
+    # target object.
+    #
+    # The target object is saved in the database. Every referenced migrated object is created,
+    # if necessary. Finally, a migration target owner object is created, if necessary.
+    #
+    # For example, suppose a migration configuration specifies the following:
+    # * the target is a +CaTissue::SpecimenCollectionGroup+
+    # * the field mapping specifies a +Participant+ MRN,
+    # * the defaults specify a +CollectionProtocol+ title and a +Site+ name
+    #
+    # The migrator attempts to fetch the protocol and site from the database. If they do not
+    # exist, then they are created. In order to create the protocol and site, the migration
+    # configuration must specify sufficient information to validate the objects before creation,
+    # as described in {#initialize}. Finally, the SCG +CollectionProtocolRegistration+ owner
+    # is created. This CPR references the migration protocol and site.
     #
     # If the +:create+ option is set, then an input record for a target object which already
     # exists in the database is noted in a debug log message and ignored rather than updated.
     #
-    # @yield (see #migrate)
-    # @yieldparam (see #migrate)
-    # @return (see #migrate)
+    # @yield [target, row] operates on the migration target
+    # @yieldparam [Resource] target the migrated target domain object
+    # @yieldparam [{Symbol => Object}] row the migration source record
     def migrate_to_database(&block)
       # migrate with save
       tm = Jinx::Stopwatch.measure { execute_save(&block) }.elapsed
@@ -44,14 +60,10 @@ module CaRuby
       end
       @database.open do |db|
         migrate do |tgt, rec|
+          # Save the target object.
           save(tgt, db)
           # Ensure that each migrated object is created if necessary.
-          @migrated.each do |obj|
-            next if obj.identifier
-            logger.debug { "The migrator is saving the migrated #{obj}..." }
-            save(obj, db)
-            logger.debug { "The migrator saved the migrated #{obj}." }
-          end
+          @migrated.each { |obj| create(obj, db) unless db.exists?(obj) }
           yield(tgt, rec) if block_given?
           db.clear
         end
@@ -62,14 +74,20 @@ module CaRuby
     # @return [Resource, nil] obj if the save is successful, nil otherwise
     def save(obj, database)
       if @create then
-        logger.debug { "Migrator creating #{obj}..." }
-        database.create(obj)
-        logger.debug { "Migrator created #{obj}." }
+        create(obj, database)
       else
         logger.debug { "Migrator saving #{obj}..." }
         database.save(obj)
         logger.debug { "Migrator saved #{obj}." }
       end
+    end
+
+    # @param [Resource] obj the domain object to create in the database
+    # @return [Resource, nil] obj if the create is successful, nil otherwise
+    def create(obj, database)
+      logger.debug { "Migrator creating #{obj}..." }
+      database.create(obj)
+      logger.debug { "Migrator created #{obj}." }
     end
 
     # @return [String] a log message for the given migration time in seconds
